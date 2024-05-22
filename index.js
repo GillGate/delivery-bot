16933e183ad4cd63fb59d7850ee241eff31b8c11
PaskeyDuet <romahaforever1@yandex.ru>
2024-05-22 13:12:43 +0300

    prettier fix, let->const, func test

--- index.js
+++ index.js
@@ -7,97 +7,97 @@ import { mainMenu } from "#bot/keyboards/general.js";
 import { db, getUserInfo } from "#bot/plugins/firebase.plugin.js";
 
 const initSessionData = {
-  user: {
-    fio: "",
-    address: "",
-  },
-  routeHistory: [],
-  order: {
-    type: "",
-    subType: "",
-    link: "",
-    size: "",
-    price: "",
-  },
+    user: {
+        fio: "",
+        address: "",
+    },
+    routeHistory: [],
+    order: {
+        type: "",
+        subType: "",
+        link: "",
+        size: "",
+        price: "",
+    },
 };
 
 const bot = new Bot(process.env.BOT_API_TOKEN);
 bot.use(
-  session({
-    initial: () => structuredClone(initSessionData),
-    // storage: adapter(db.collection("sessions")),
-  })
+    session({
+        initial: () => structuredClone(initSessionData),
+        // storage: adapter(db.collection("sessions")),
+    })
 );
 bot.use(hydrate());
 bot.use(order);
 
 bot.api.setMyCommands([
-  {
-    command: "start",
-    description: "Запуск бота",
-  },
+    {
+        command: "start",
+        description: "Запуск бота",
+    },
 ]);
 
 async function sendStartMessage(ctx, errorMode = false) {
-  ctx.session.routeHistory = [];
-  ctx.session.order = structuredClone(initSessionData.order);
-  ctx.session.conversation = {};
+    ctx.session.routeHistory = [];
+    ctx.session.order = structuredClone(initSessionData.order);
+    ctx.session.conversation = {};
 
-  let helloText = `Привет 🚸\n\n`;
-  helloText += `Я Kul2Bot и я могу помочь тебе сделать заказ оригинальных вещей с Poizon, а также подсказать, что именно заказать, исходя из модных тенденций о которых пишет наш журнал.\n\n`;
-  helloText += `Что тебя интересует? 🫡`;
+    let helloText = `Привет 🚸\n\n`;
+    helloText += `Я Kul2Bot и я могу помочь тебе сделать заказ оригинальных вещей с Poizon, а также подсказать, что именно заказать, исходя из модных тенденций о которых пишет наш журнал.\n\n`;
+    helloText += `Что тебя интересует? 🫡`;
 
-  await ctx.reply(helloText, {
-    reply_markup: mainMenu,
-  });
+    await ctx.reply(helloText, {
+        reply_markup: mainMenu,
+    });
 
-  let user = ctx.session.user;
-  if (user.fio === "" || user.address === "") {
-    try {
-      let userInfo = await getUserInfo(ctx.from.id);
+    let user = ctx.session.user;
+    if (user.fio === "" || user.address === "") {
+        try {
+            let userInfo = await getUserInfo(ctx.from.id);
 
-      if (userInfo.exists) {
-        ctx.session.user = userInfo.data();
-      }
-    } catch (e) {
-      console.error(e);
+            if (userInfo.exists) {
+                ctx.session.user = userInfo.data();
+            }
+        } catch (e) {
+            console.error(e);
+        }
     }
-  }
 
-  console.log("session", ctx.session);
+    console.log("session", ctx.session);
 
-  if (ctx?.callbackQuery && !errorMode) {
-    await ctx.answerCallbackQuery();
-  }
+    if (ctx?.callbackQuery && !errorMode) {
+        await ctx.answerCallbackQuery();
+    }
 }
 
 bot.command("start", async (ctx) => await sendStartMessage(ctx));
 bot.callbackQuery("main_menu", async (ctx) => await sendStartMessage(ctx));
 
 bot.callbackQuery("back", async (ctx) => {
-  await ctx.session.routeHistory.pop(); // фальшивка ёбанная
-  const routeParams = await ctx.session.routeHistory.pop();
+    await ctx.session.routeHistory.pop(); // фальшивка ёбанная
+    const routeParams = await ctx.session.routeHistory.pop();
 
-  await ctx.editMessageText(routeParams.text, {
-    reply_markup: routeParams.reply_markup,
-  });
-  await ctx.answerCallbackQuery();
+    await ctx.editMessageText(routeParams.text, {
+        reply_markup: routeParams.reply_markup,
+    });
+    await ctx.answerCallbackQuery();
 });
 
 bot.catch(async (err) => {
-  const ctx = err.ctx;
-  console.error(`Error while handling update ${ctx.update.update_id}`);
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}`);
 
-  const e = err.error;
-  if (e instanceof GrammyError) {
-    console.error("Error in request:", e.description);
-    // await sendStartMessage(ctx, true);
-  } else if (e instanceof HttpError) {
-    console.log("Could not contact Telegram:", e);
-  } else {
-    console.error("Unknown error:", e);
-    await sendStartMessage(ctx, true);
-  }
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+        // await sendStartMessage(ctx, true);
+    } else if (e instanceof HttpError) {
+        console.log("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+        await sendStartMessage(ctx, true);
+    }
 });
 
 bot.start();
